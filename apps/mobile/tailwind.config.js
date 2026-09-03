373a5f6ada56bca03292c8a18d001248bafd4422
jvitorsantana <jvitorsantana@outlook.com.br>
2026-09-03 08:22:24 -0300

    feat: configuração de pastas e nativewind

---
+++ apps/mobile/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/App.tsx",
+    "./src/components/**/*.{.tsx}"
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
